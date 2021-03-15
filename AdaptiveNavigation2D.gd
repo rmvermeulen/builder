9637377eace7dcd844923b0bccd34510279ee84b
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-03-15 13:08:08 +0100

    Implement cutting navpolys around collision bodies, probably

--- AdaptiveNavigation2D.gd
+++ AdaptiveNavigation2D.gd
@@ -31,10 +31,39 @@ func adapt() -> void:
 	for i in poly.navpoly.get_outline_count():
 		outlines.append(poly.navpoly.get_outline(i))
 
+	var results = []
 	for outline in outlines:
 		for c in collisions:
 			var polygon = polygon_from_body(c.collider)
-			prints(outline, polygon)
+			for result in GoostGeometry2D.clip_polygons(outline, polygon):
+				results.append(result)
+
+	var navpoly := NavigationPolygon.new()
+	for i in results.size():
+		var child
+		if i < get_child_count():
+			child = get_child(i)
+		else:
+			child = NavigationPolygonInstance.new()
+			add_child(child)
+		navpoly.add_outline(results[i])
+	navpoly.make_polygons_from_outlines()
+	prints('outlines:', navpoly.get_outline_count())
+	prints('polygons:', navpoly.get_polygon_count())
+	poly.navpoly = navpoly
+	update()
+
+
+func _draw() -> void:
+	var np := poly.navpoly
+	var vs := np.get_vertices()
+	for i in np.get_polygon_count():
+		var ids :=  np.get_polygon(i)
+		var p :=PoolVector2Array  ()
+		for id in ids:
+			p.append(vs[id])
+		p.append(p[p.size() - 1])
+		draw_polyline(p, Color.white, 1, true)
 
 
 static func polygon_from_body(body: PhysicsBody2D) -> PoolVector2Array:
@@ -42,25 +71,31 @@ static func polygon_from_body(body: PhysicsBody2D) -> PoolVector2Array:
 	var rid: RID = body.get_rid()
 	var s = phs.body_get_shape(rid, 0)
 	var t = phs.body_get_shape_transform(rid, 0)
-	var m = phs.body_get_shape_metadata(rid, 0)
+	# var m = phs.body_get_shape_metadata(rid, 0)
 	var data = phs.shape_get_data(s)
-	match phs.shape_get_type(s):
-		phs.SHAPE_CIRCLE:
-			prints("circle, radius =", data)
-			assert(typeof(data) == TYPE_REAL)
-			return GoostGeometry2D.circle(float(data))
-		phs.SHAPE_RECTANGLE:
-			prints("rect, extents =", data)
-			assert(typeof(data) == )
-		phs.SHAPE_CONVEX_POLYGON:
-			prints("convex, points =", data)
-		phs.SHAPE_CONCAVE_POLYGON:
-			prints("concave, points =", data)
-		var type:
-			prints("unknown type", type, data)
-	prints('transform', t)
-	if m:
-		prints('meta', m)
 
-	var result: PoolVector2Array = []
-	return result
+	var polygon := PoolVector2Array()
+	match [phs.shape_get_type(s), typeof(data)]:
+		[phs.SHAPE_CIRCLE, TYPE_REAL]:
+			prints("circle, radius =", data)
+			polygon = GoostGeometry2D.circle(float(data))
+
+		[phs.SHAPE_RECTANGLE, TYPE_VECTOR2]:
+			prints("rect, extents =", data)
+			var half: Vector2= data * 0.5
+			polygon = PoolVector2Array([
+				-half,
+				Vector2(half.x, -half.y),
+				half,
+				Vector2(-half.x, half.y),
+			])
+
+		[_, TYPE_VECTOR2_ARRAY]:
+			prints("convex/concave polygon, points =", data)
+			polygon = data
+
+		_:
+			prints("unknown type", data, typeof(data))
+	for i in polygon.size():
+		polygon[i] = (body.transform * t).xform(polygon[i])
+	return polygon
